a848c194cfc4575a7b8a8b5cd64119fd7eb944e6
Mark Dencler <mdencler@gmail.com>
2026-03-26 17:19:51 -0400

    Updating Module 03 materials.

---
+++ M03_Input_Polling.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Create window
+    width, height = 480, 360
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Hello World")
+
+    # Set up font
+    font = pygame.font.SysFont(None, 48)
+
+    # Main loop
+    running = True
+    while running:
+        # Handle events
+        for event in pygame.event.get():
+            
+            # Render the text for each event and its associated attributes.
+            print(pygame.event.event_name(event.type))  # Console Info Display
+
+            # For each box of text, create a 'Surface' with the desired contents.
+            text_event       = font.render(pygame.event.event_name(event.type), True, COLOR_BLACK)
+            text_attribute_1 = font.render(str(event.type)                    , True, COLOR_BLACK)
+            
+            # Process the QUIT event.
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill background
+        screen.fill(COLOR_WHITE)
+
+        # Draw text boxes
+        text_rect_event = text_event.get_rect(center=(width // 2, int(height * 0.25)))
+        screen.blit(text_event, text_rect_event)
+
+        text_rect_event_A1 = text_attribute_1.get_rect(center=(width // 2, int(height * 0.75)))
+        screen.blit(text_attribute_1, text_rect_event_A1)
+
+        # Update display (page flip)
+        pygame.display.flip()
+
+    # Clean up
+    pygame.quit()
+
+main()
---
+++ docs/Lesson_03_Input_Polling.md
@@ -0,0 +1,42 @@
+### Overview
+<details>
+    <summary>
+    Lesson Summary
+    </summary>
+
+```
+HCPS Pygame Curricular Project
+Title: Lesson 03 - Input Polling
+Description: This lesson reviews the basics of polling for input events in Pygame.
+Author: Mark Dencler (mdencler@harford.edu)
+Date: 2026-03-26
+```
+</details>
+
+INPUT POLLING INTRO
+
+<b>M03_Input_Polling.py</b>
+```py
+#CODE
+```
+
+### <u>Event Types</u>
+| Event Name      | Event ID | OPTION 01 | OPTION 02 |
+|-----------------|----------|-----------|-----------|
+| MouseMotion     |
+| MouseButtonDown |
+| MouseButtonUp   |
+| KeyDown         |
+| KeyUp           |
+| TextInput       |
+| Window          |
+| ActiveEvent     |
+
+
+Let's start by breaking down the beginning part of this script.
+
+```py
+#CODE
+```
+
+Pygame is a specialized code library that runs within the Python interpreted environment.  Pygame provides access to a series of library functions that perform tasks related to 
