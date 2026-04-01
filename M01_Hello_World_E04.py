517f61f822fbd3a26fc890b0a32107396cf0b1b2
Mark Dencler <mdencler@gmail.com>
2026-04-01 18:58:48 -0400

    modifed A01 solution files.. added pride rectangles example

--- M01_Hello_World_E04.py
+++ M01_Hello_World_E04.py
@@ -16,11 +16,11 @@ DISPLAY_HEIGHT = 360
 COLOR_WHITE   = (255, 255, 255)       # Custom Colors
 COLOR_BLACK   = (0, 0, 0)
 COLOR_RED     = (255, 0, 0)
-COLOR_ORANGE  = (177, 177, 0)
-COLOR_YELLOW  = (0, 177, 177)
-COLOR_GREEN   = (0, 255, 0)
+COLOR_ORANGE  = (255, 165, 0)
+COLOR_YELLOW  = (255, 255, 0)
+COLOR_GREEN   = (0, 128, 0)
 COLOR_BLUE    = (0, 0, 255)
-COLOR_PURPLE  = (177, 0, 177)
+COLOR_PURPLE  = (148, 0, 211)
 
 def main():
     # 1
@@ -37,21 +37,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        # --- Drawing Logic ---
     
         # Fill the background with white
         screen.fill(COLOR_WHITE)
     
         # Draw the Rectangles
         # pygame.draw.rect(surface, color, [x, y, width, height]
-        # RED
         pygame.draw.rect(screen, COLOR_RED,    [0 * (DISPLAY_WIDTH // 6), 0 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])
         pygame.draw.rect(screen, COLOR_ORANGE, [1 * (DISPLAY_WIDTH // 6), 1 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
-        pygame.draw.rect(screen, COLOR_YELLOW, [2 * (DISPLAY_WIDTH // 6), 2 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
-        pygame.draw.rect(screen, COLOR_GREEN,  [3 * (DISPLAY_WIDTH // 6), 3 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
-        pygame.draw.rect(screen, COLOR_BLUE,   [4 * (DISPLAY_WIDTH // 6), 4 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
-        pygame.draw.rect(screen, COLOR_PURPLE, [5 * (DISPLAY_WIDTH // 6), 5 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
+        #pygame.draw.rect(screen, COLOR_YELLOW, [2 * (DISPLAY_WIDTH // 6), 2 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
+        #pygame.draw.rect(screen, COLOR_GREEN,  [3 * (DISPLAY_WIDTH // 6), 3 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
+        #pygame.draw.rect(screen, COLOR_BLUE,   [4 * (DISPLAY_WIDTH // 6), 4 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
+        #pygame.draw.rect(screen, COLOR_PURPLE, [5 * (DISPLAY_WIDTH // 6), 5 * (DISPLAY_HEIGHT // 6), (DISPLAY_WIDTH // 6), DISPLAY_HEIGHT // 6])           
     
         # Update the display to show what we drew
         pygame.display.flip()
